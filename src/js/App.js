f63d2b9683d2caa8338c7845e202be4cf9315bc8
Edgar Diaz <ed35376@gmail.com>
2023-05-06 20:19:44 -0400

    ToDo list Created

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./component/pages/Home.jsx";
+
+
+function App (){
+    return(
+        <>
+        <Home/>
+            </>
+    )
+}
+export default App
